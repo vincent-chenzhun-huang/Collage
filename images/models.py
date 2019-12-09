94c3de2b6c0548bcab6380d78aa5e110a1f72d39
Vincent Huang <chenzhun.huang@mail.mcgill.ca>
2019-12-09 18:59:40 -0500

    Add configurations and setup, build up the user system

---
+++ images/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+class Image(models.Model):
+    user = models.ForeignKey(User,
+                             related_name='images_created',
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+                            blank=True)
+    url = models.URLField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True,
+                               db_index=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Image, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.title
