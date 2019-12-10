f997d15beabc1ac47cfdd0dccb5bb41115239d5d
Vincent Huang <chenzhun.huang@mail.mcgill.ca>
2019-12-10 00:13:40 -0500

    fixed syntax issue.

--- images/forms.py
+++ images/forms.py
@@ -18,7 +18,7 @@ class ImageCreateForm(forms.ModelForm):
         valid_extensions = ['jpg', 'jpeg']
         extensions = url.rsplit('.', 1)[1].lower()
         if extensions not in valid_extensions:
-            raise forms.ValidationError('The given URL does not ' \
+            raise forms.ValidationError('The given URL does not '
                                         'match valid image extensions.')
         return url
 
